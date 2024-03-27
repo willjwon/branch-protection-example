1e010ae4a66e6dc2ee9352edf9052030ec401388
Will Won <william.won@gatech.edu>
2024-03-27 19:02:54 -0400

    Update test3

--- test3.cpp
+++ test3.cpp
@@ -3,5 +3,11 @@
 int main() {
     std::cout << "Hello, world!" << std::endl;
 
+    auto sum = 0;
+    for (auto i = 1; i <= 10; i++) {
+        sum += i;
+    }
+    std::cout << "Sum: " << sum << std::endl;
+
     return 0;
 }
